1cc7c2f72a1362dfd7a2512a016f48767f1ef438
unknown <mosharafctg111@gmail.com>
2022-12-13 22:51:01 +0600

    added the get function

--- index.js
+++ index.js
@@ -2,11 +2,37 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // midleware 
 
 app.use(express.json())
 app.use(cors())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.uujg3og.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const projectsCollection = client.db('developerPortfolio-1').collection('projects')
+        app.get('/projects',async (req, res) =>{
+            const query = {}
+            const result = await projectsCollection.find(query).toArray()
+            res.json(result)
+        })
+
+    }
+    finally{
+
+    }
+
+}
+run().catch(err=> console.err(err))
+
+
 
 app.get('/', (req, res) => {
   res.send('Foysal portfolio server is running ')
